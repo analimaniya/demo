02014c83c28ce0897437138ab3844963a6106a75
analimaniya <153978720+analimaniya@users.noreply.github.com>
2023-12-18 15:59:52 +0530

    test

--- app/Http/Controllers/demoController.php
+++ app/Http/Controllers/demoController.php
@@ -8,7 +8,8 @@ class demoController extends Controller
 {
     public function index(){
         $array = [
-            [
+            [ 
+                
                 'name' => 'php',
                 'framework' => 'laravel'
             ],
