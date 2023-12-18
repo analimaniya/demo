fc3dad945c892c325e5d7e310c7e5523972b5211
analimaniya <153978720+analimaniya@users.noreply.github.com>
2023-12-18 15:58:00 +0530

    First commit

---
+++ app/Http/Controllers/demoController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class demoController extends Controller
+{
+    public function index(){
+        $array = [
+            [
+                'name' => 'php',
+                'framework' => 'laravel'
+            ],
+            [
+                'name'=> 'javaScript',
+                'framework' => 'express.js'     
+            ]
+        ];
+
+        return response()->json([
+            'status' =>true ,
+            'message' => '2 record find it.',
+            'data'=> $array
+        ],200);
+    }
+}
